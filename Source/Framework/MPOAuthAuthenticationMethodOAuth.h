8625e72378efd7ea55c76f4891857569f514aecc
Christian Beer <christian.beer@chbeer.de>
2013-07-29 15:46:59 +0200

    Added method to delegate

--- Source/Framework/MPOAuthAuthenticationMethodOAuth.h
+++ Source/Framework/MPOAuthAuthenticationMethodOAuth.h
@@ -45,6 +45,8 @@ extern NSString * const MPOAuthNotificationRequestTokenRejected;*/
 - (NSString *)oauthVerifierForCompletedUserAuthorization;
 - (void)authenticationDidFailWithError:(NSError *)error;
 
+- (void) authentication:(MPOAuthAuthenticationMethodOAuth*)auth didFinishWithData:(NSData*)data;
+
 - (NSArray*) additionalParametersForRequestTokenRequest;
 
 - (void) openOAuthURL:(NSURL*)url;
